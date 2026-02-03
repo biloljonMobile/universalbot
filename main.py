3b19ce0fef08b917215c4a9428e206be7d2cf2ce
biloljon <zetatensai483@gmail.com>
2026-02-03 20:00:39 +0500

    removed proxy

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import logging
 from aiogram import Bot, Dispatcher
 from config import TOKEN
 from aiogram.client.default import DefaultBotProperties
-from aiogram.client.session.aiohttp import AiohttpSession
 
 from database.crud import init_db
 from handlers import start, instagram, translate, weather, prayer, chatgpt, pdf, admin
@@ -12,11 +11,9 @@ from handlers import start, instagram, translate, weather, prayer, chatgpt, pdf,
 
 logging.basicConfig(level=logging.INFO)
 
-PROXY_URL = "http://proxy.server:3128"
-session = AiohttpSession(proxy=PROXY_URL)
 
 async def main():
-    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"), session=session)
+    bot = Bot(token=TOKEN, default=DefaultBotProperties(parse_mode="HTML"))
     dp = Dispatcher()
 
     init_db()
